c24eae49e43a5f00413a4c724f5e76c13040b854
sazal <sazals48@gmail.com>
2024-01-23 14:45:15 +0545

    get single product and product search

--- server/src/controllers/productControllers/productController.js
+++ server/src/controllers/productControllers/productController.js
@@ -3,21 +3,40 @@ const Product = require("../../models/productModel/products");
 
 const getProducts = asyncHandler(async (req, res) => {
   let product = await Product.find();
-  if (!product) {
+  if (product.length === 0) {
     res.status(400);
-    throw new Error("Something went wrong")
+    throw new Error("No products found");
   }
   return res.status(200).send(product);
 });
 
+const searchProducts = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        product_name: { $regex: req.query.search, $options: 'i' },
+      }
+    : {};
+  const productId = req.params.id;
+  const products = await Product.find({ ...keyword, _id: { $ne: productId } });
+  res.send(products);
+});
+
+const getSingleProduct = asyncHandler(async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(400).json({ err: "Something went wrong" });
+  }
+  res.send(product);
+});
+
 const addProducts = asyncHandler(async (req, res) => {
   const { product_name, description, price, category } = req.body;
-  const image  = req.file.path;
+  const image = req.file.path;
 
   if (!product_name || !description || !price) {
     res.status(400);
     throw new Error("Please add a name, description and price");
-  };
+  }
 
   const productExists = await Product.findOne({ product_name });
 
@@ -31,7 +50,7 @@ const addProducts = asyncHandler(async (req, res) => {
     description,
     price,
     image,
-    category
+    category,
   });
 
   if (product) {
@@ -40,7 +59,6 @@ const addProducts = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Invalid product data");
   }
-  
 });
 
 const updateProduct = asyncHandler(async (req, res) => {
@@ -52,7 +70,7 @@ const updateProduct = asyncHandler(async (req, res) => {
     description,
     price,
     image,
-    category
+    category,
   });
 
   if (!updateProduct) {
@@ -66,14 +84,16 @@ const updateProduct = asyncHandler(async (req, res) => {
 const deleteProduct = asyncHandler(async (req, res) => {
   let deleteProduct = await Product.findByIdAndDelete(req.params.id);
   if (!deleteProduct) {
-    return res.status(400).json({ error: "something went wrong" });
+    return res.status(404).json({ error: "Product not found" });
   }
   res.json({ success: "Deleted successfully" });
 });
 
 module.exports = {
   getProducts,
+  searchProducts,
+  getSingleProduct,
   addProducts,
   updateProduct,
-  deleteProduct
-}
+  deleteProduct,
+};
--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../config/fileUpload');
 const { protect } = require('../middleware/authMiddleware');
-const { addProducts, updateProduct, getProducts, deleteProduct } = require('../controllers/productControllers/productController');
+const { addProducts, updateProduct, getProducts, deleteProduct, getSingleProduct, searchProducts,  } = require('../controllers/productControllers/productController');
 
 router.post('/add-product', upload.single('image'), addProducts);
-router.get('/get-product', getProducts);
+router.get('/get-products', getProducts);
+router.get('/get-product/:id', getSingleProduct);
+router.get('/search-product', searchProducts);
 router.put('/update-product/:id', updateProduct);
 router.delete('/delete-product/:id', deleteProduct);
 
