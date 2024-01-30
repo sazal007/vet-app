304163880cd456f071a9ca7072ad532e3225df34
sazal <sazals48@gmail.com>
2024-01-30 19:48:46 +0545

    routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
+import MyRoutes from "./routes/Routes"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <MyRoutes />
     </>
   )
 }
---
+++ client/src/routes/Routes.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+const MyRoutes = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default MyRoutes
