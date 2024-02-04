cdf507ab4fa2619fdd12d8bcda2de495801eb3cb
sazal <sazals48@gmail.com>
2024-02-04 21:25:08 +0545

    doctor update, delete, approve status

--- server/src/controllers/usersControllers/doctorController.js
+++ server/src/controllers/usersControllers/doctorController.js
@@ -1,33 +1,25 @@
-const asyncHandler = require('express-async-handler');
-const Doctor = require('../../models/userModel/doctor');
-const User = require('../../models/userModel/user');
-
-const getDoctor = asyncHandler(async (req, res) => {
-  const doctor = await Doctor.findOne({ userId: req.user._id });
-  if (!doctor) {
-    res.status(400);
-    throw new Error('Doctor not found');
-  }
-  res.status(200).json(doctor);
-});
+const asyncHandler = require("express-async-handler");
+const Doctor = require("../../models/userModel/doctor");
+const User = require("../../models/userModel/user");
 
 const getAllDoctors = asyncHandler(async (req, res) => {
   const doctors = await Doctor.find();
   if (!doctors) {
     res.status(400);
-    throw new Error('Doctors not found');
+    throw new Error("Doctors not found");
   }
   res.status(200).json(doctors);
 });
 
 const registerDoctor = asyncHandler(async (req, res) => {
   const newDoctor = await Doctor.create({
-    ...req.body, status: "pending"
+    ...req.body,
+    status: "pending",
   });
   res.status(200).json({
     success: true,
     message: "Doctor Account Applied Successfully",
-    data: newDoctor
+    data: newDoctor,
   });
 
   const adminUser = await User.findOne({ role: "admin" });
@@ -49,9 +41,60 @@ const registerDoctor = asyncHandler(async (req, res) => {
   });
 });
 
+const deleteDoctor = asyncHandler(async (req, res) => {
+  const doctor = await Doctor.findByIdAndDelete(req.params.id);
+  if (!doctor) {
+    return res.status(400).json({ error: "something went wrong" });
+  }
+  res.json({ message: "Deleted successfully" });
+});
+
+const updateDoctor = asyncHandler(async (req, res) => {
+  const doctor = await Doctor.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!doctor) {
+    return res.status(400).json({ error: "something went wrong" });
+  }
+  res.json({ message: "Updated successfully" });
+});
+
+const getDoctorDetails = asyncHandler(async (req, res) => {
+  const doctor = await Doctor.findById(req.params.id);
+  if (!doctor) {
+    return res.status(400).json({ error: "something went wrong" });
+  }
+  res.status(200).send({
+    success: true,
+    message: "doctor data fetch success",
+    data: doctor,
+  });
+});
+
+const changeStatus = asyncHandler(async (req, res) => {
+  const { doctorId, status } = req.body;
+  const doctor = await doctorModel.findByIdAndUpdate(doctorId, { status });
+  const user = await userModel.findOne({ _id: doctor.userId });
+  const notification = user.notification;
+  notification.push({
+    type: "doctor-account-request-updated",
+    message: `Your Doctor Account Request Has ${status} `,
+    onClickPath: "/notification",
+  });
+  user.isDoctor = status === "approved" ? true : false;
+  await user.save();
+  res.status(201).send({
+    success: true,
+    message: "Account Status Updated",
+    data: doctor,
+  });
+});
 
 module.exports = {
-  getDoctor,
   getAllDoctors,
-  registerDoctor
-}
+  registerDoctor,
+  deleteDoctor,
+  updateDoctor,
+  getDoctorDetails,
+  changeStatus,
+};
--- server/src/routes/doctorRoute.js
+++ server/src/routes/doctorRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerDoctor, getDoctor, getAllDoctors } = require('../controllers/usersControllers/doctorController');
+const { registerDoctor, getAllDoctors, deleteDoctor, updateDoctor, getDoctorDetails, changeStatus } = require('../controllers/usersControllers/doctorController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.get('/get-doctor', getDoctor);
+router.get('/get-doctor', getDoctorDetails);
 router.get('/all-doctors', getAllDoctors);
 router.post('/register-doctor', registerDoctor);
+router.delete('/delete-doctor/:id', deleteDoctor);
+router.put('/update-doctor/:id', updateDoctor);
+router.post('/change-status', changeStatus);
 
 module.exports = router
