b19bd511716e1b75c82c0faa3be2834702a90110
sazal <sazals48@gmail.com>
2024-03-02 17:21:36 +0545

    404 error route

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -2,16 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../pages/misc/home'
 import Login from '../pages/Auth/login'
 import Register from '../pages/Auth/register'
+import ChatProvider from '../context/chatProvider'
+import NotFoundPage from '../pages/misc/NotFoundPage'
 
 const MyRoutes = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<Register />} />
-        </Routes>
+        <ChatProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/sign-up" element={<Register />} />
+            <Route path="*" element={<NotFoundPage />} /> {/* Catch-all route */}
+          </Routes>
+        </ChatProvider>
       </BrowserRouter>
     </>
   )
