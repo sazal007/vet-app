faee874c4883bce1931973365edf47187e700e79
sazal <sazals48@gmail.com>
2024-03-02 20:57:11 +0545

    context handler

---
+++ client/src/context/chatProvider.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const chatContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+const ChatProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState([]);
+  const [notification, setNotification] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(data);
+
+    if (!data) {
+      navigate('/');
+    }
+  }, [navigate])
+
+  return (
+    <chatContext.Provider value={{ user, setUser, selectedChat, setSelectedChat, chats, setChats, notification, setNotification }}>
+      {children}
+    </chatContext.Provider>
+  );
+}
+
+export const ChatState = () => {
+  return useContext(chatContext);
+}
+
+
+export default ChatProvider;
