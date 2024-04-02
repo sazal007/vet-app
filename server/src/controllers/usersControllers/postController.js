457e2fc0fd38da8d2c68b169498f4b805f1f0c8e
sazal <sazals48@gmail.com>
2024-04-02 21:13:25 +0545

    post-get comments & likes

--- server/src/controllers/usersControllers/postController.js
+++ server/src/controllers/usersControllers/postController.js
@@ -30,12 +30,15 @@ const createPost = asyncHandler(async (req, res) => {
 });
 
 const getAllPosts = asyncHandler(async (req, res) => {
-  const posts = await Post.find();
+  const posts = await Post.find().populate("user", "name email");
   res.status(200).json(posts);
 });
 
 const getSinglePost = asyncHandler(async (req, res) => {
-  const post = await Post.findById(req.params.id);
+  const post = await Post.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
   if (!post) {
     return res.status(400).json({ error: "something went wrong" });
   }
@@ -74,10 +77,77 @@ const updatePost = asyncHandler(async (req, res) => {
   res.status(200).json(updatedPost);
 });
 
+const addComments = asyncHandler(async (req, res) => {
+  const { text } = req.body;
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    return res.status(400).json({ error: "Post not found." });
+  }
+  // Create a new comment object
+  const comment = {
+    user: req.user._id,
+    text,
+  };
+  post.comments.push(comment);
+  await post.save();
+  res.status(200).json(post);
+});
+
+const getComments = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id).populate(
+    "comments.user",
+    "name email"
+  );
+  if (!post) {
+    return res.status(400).json({ error: "Post not found." });
+  }
+  res.status(200).json(post.comments);
+});
+
+const likePost = asyncHandler(async (req, res) => {
+  const postId = req.params.id;
+  const userId = req.user._id;
+
+  const post = await Post.findById(postId);
+  if (!post) {
+    return res.status(400).json({ error: "Post not found." });
+  }
+  // Check if the post has already been liked by this user
+  const alreadyLiked = post.likes.find(
+    (like) => like.user.toString() === userId.toString()
+  );
+  if (alreadyLiked) {
+    // If already liked, remove the like
+    post.likes = post.likes.filter(
+      (like) => like.user.toString() !== userId.toString()
+    );
+  } else {
+    // If not liked, add a new like
+    post.likes.push({ user: userId });
+  }
+
+  await post.save();
+  res.json(post.likes);
+});
+
+const getLikes = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id)
+    .select("likes -_id")
+    .populate("likes.user", "name email");
+  if (!post) {
+    return res.status(400).json({ error: "Post not found." });
+  }
+  res.status(200).json(post.likes);
+});
+
 module.exports = {
   createPost,
   getAllPosts,
   getSinglePost,
   deletePost,
   updatePost,
+  addComments,
+  getComments,
+  likePost,
+  getLikes,
 };
--- server/src/routes/postRoute.js
+++ server/src/routes/postRoute.js
@@ -8,6 +8,10 @@ const {
   getSinglePost,
   deletePost,
   updatePost,
+  addComments,
+  getComments,
+  likePost,
+  getLikes,
 } = require("../controllers/usersControllers/postController");
 
 router.post("/create-post", protect, upload.array("images", 5), createPost);
@@ -15,5 +19,9 @@ router.get("/get-post", protect, getAllPosts);
 router.get("/get-post/:id", protect, getSinglePost);
 router.delete("/delete-post/:id", protect, deletePost);
 router.put("/update-post/:id", protect, updatePost);
+router.post("/comments/:id", protect, addComments);
+router.get("/comments/:id", protect, getComments);
+router.post("/like-post/:id", protect, likePost);
+router.get("/like-post/:id", protect, getLikes);
 
 module.exports = router;
