979472251603fd90dc924faf806dea96671e47f4
sazal <sazals48@gmail.com>
2024-04-03 22:17:59 +0545

    api call for creating and update pet profile

--- client/src/apis/petprofile/petsProfile.js
+++ client/src/apis/petprofile/petsProfile.js
@@ -18,3 +18,43 @@ export const getProfile = async () => {
     console.log(err);
   }
 };
+
+export const createProfile = async (formData) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const token = userInfo?.token;
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/pet/create-profile`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateProfile = async (id, formData) => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const token = userInfo?.token;
+  try {
+    const response = await axios.put(
+      `${import.meta.env.VITE_BACKEND_URL}/pet/update-profile/${id}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
