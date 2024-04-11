a6f0e047b3d4f02d54c3b51b6591d193cc8faaea
sazal <sazals48@gmail.com>
2024-04-11 20:02:16 +0545

    apis for viewing appointments

--- client/src/apis/vet/appointment.js
+++ client/src/apis/vet/appointment.js
@@ -39,3 +39,65 @@ export const checkAvailability = async (data) => {
     console.log(err);
   }
 };
+
+export const getUserAppointments = async () => {
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+  const token = user?.token;
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/user/get-appointments`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getDocAppoiments = async () => {
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+  const token = user?.token;
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_URL}/doctor/get-doc-appoitments`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const appointmentSatus = async (record, status) => {
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+  const token = user?.token;
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_BACKEND_URL}/doctor/accept-appointment`,
+      {
+        appointmentsId: record._id,
+        userId: record.userId,
+        status,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
